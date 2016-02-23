75154cab0f4fd3250176d8961b34fa841172b8a7
elynna <elynna.yang@gmail.com>
2016-02-23 13:53:01 +1300

    Added contact sensor plugin - prints out contact values.

---
+++ contact_plugin/src/contact_plugin.cpp
@@ -0,0 +1,62 @@
+#include "contact_plugin/contact_plugin.hh"
+
+using namespace gazebo;
+GZ_REGISTER_SENSOR_PLUGIN(ContactPlugin)
+
+// Constructor
+ContactPlugin::ContactPlugin() : SensorPlugin()
+{
+}
+// Destructor
+ContactPlugin::~ContactPlugin()
+{
+}
+
+// Load function gets a pointer to the contact sensor through the _sensor parameter
+void ContactPlugin::Load(sensors::SensorPtr _sensor, sdf::ElementPtr /*_sdf*/)
+{
+  // Get the parent sensor.
+  this->parentSensor =
+    boost::dynamic_pointer_cast<sensors::ContactSensor>(_sensor);
+
+  std::cout << "TEST LOAD: "<< this->parentSensor << std::endl;
+  // Make sure the parent sensor is valid.
+  if (!this->parentSensor)
+  {
+    gzerr << "ContactPlugin requires a ContactSensor.\n";
+    return;
+  }
+
+  // Connect to the sensor update event.
+  this->updateConnection = this->parentSensor->ConnectUpdated(
+      boost::bind(&ContactPlugin::OnUpdate, this));
+
+  // Make sure the parent sensor is active.
+  this->parentSensor->SetActive(true);
+}
+
+// OnUpdate is called when the contact sensor is updated. The values are printed out to terminal.
+void ContactPlugin::OnUpdate()
+{
+  // Get all the contacts.
+  msgs::Contacts contacts;
+  contacts = this->parentSensor->GetContacts();
+  for (unsigned int i = 0; i < contacts.contact_size(); ++i)
+  {
+    std::cout << "Collision between[" << contacts.contact(i).collision1()
+              << "] and [" << contacts.contact(i).collision2() << "]\n";
+
+    for (unsigned int j = 0; j < contacts.contact(i).position_size(); ++j)
+    {
+      std::cout << j << "  Position:"
+                << contacts.contact(i).position(j).x() << " "
+                << contacts.contact(i).position(j).y() << " "
+                << contacts.contact(i).position(j).z() << "\n";
+      std::cout << "   Normal:"
+                << contacts.contact(i).normal(j).x() << " "
+                << contacts.contact(i).normal(j).y() << " "
+                << contacts.contact(i).normal(j).z() << "\n";
+      std::cout << "   Depth:" << contacts.contact(i).depth(j) << "\n";
+    }
+  }
+}
